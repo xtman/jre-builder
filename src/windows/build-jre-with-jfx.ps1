27f20431ead697e44585bf3d614c1c5805fbbf1a
Wei Liu <wliu1976@gmail.com>
2019-10-23 10:40:51 +1100

    Tested on Windows

--- src/windows/build-jre-with-jfx.ps1
+++ src/windows/build-jre-with-jfx.ps1
@@ -1,49 +1,52 @@
 # Environment variables: JDK_HOME and JAVAFX_SDK_HOME must be set. You can uncomment and edit the two lines below to set them.
-# $Env:JDK_HOME = "C:\Users\User\Downloads\openjdk-13.0.1"
-# $Env:JAVAFX_JMODS_HOME = "C:\Users\User\Downloads\javafx-jmods-13.0.1"
+ $Env:JDK_HOME = "C:\Users\wliu5\Downloads\jdk-13.0.1"
+ $Env:JAVAFX_JMODS_HOME = "C:\Users\wliu5\Downloads\javafx-jmods-13.0.1"
 
 if (-Not (Test-Path Env:JDK_HOME)) {
-    Write-Output "Environment variable: JDK_HOME is not set."
+    Write-Error "Error: Environment variable: JDK_HOME is not set."
     Exit 1
 }
 
 if (-Not (Test-Path Env:JAVAFX_JMODS_HOME)) {
-    Write-Output "Environment variable: JAVAFX_SDK_HOME is not set."
+    Write-Error "Error: Environment variable: JAVAFX_SDK_HOME is not set."
     Exit 2
 }
 
 # check if java exists
-JAVA="$Env:JDK_HOME\bin\java"
+$JAVA="$Env:JDK_HOME\bin\java.exe"
 if (-Not (Test-Path $JAVA)) {
-    Write-Output "$JAVA does not exist"
+    Write-Error "Error: $JAVA does not exist"
     Exit 3
 }
 
 # check if jlink exists
-JLINK="$Env:JDK_HOME\bin\jlink"
+$JLINK="$Env:JDK_HOME\bin\jlink.exe"
 if (-Not (Test-Path $JLINK)) {
-    Write-Output "$JLINK does not exist"
+    Write-Error "Error: $JLINK does not exist"
     Exit 4
 }
 
+# 
+$Env:PATH = $Env:JDK_HOME + "\bin;" + $Env:PATH
+
 # parse Java version
-$VERSION_LINE = (java -version 2>&1 | Select-String version)
+$VERSION_LINE = (& $JAVA -version 2>&1 | Select-String version)
 $NAME = ($VERSION_LINE | %{$_.Line.Split(' ')[0];})
 $VERSION = ($VERSION_LINE | %{$_.Line.Split(' ')[2].Replace("`"", "");})
 $JRE_NAME = $NAME + "-" + $VERSION + "-jre"
 
 # get all moudles
 $MODULES = "javafx.controls"
-$JAVA --list-modules | ForEach-Object { 
+& $JAVA --list-modules | ForEach-Object { 
     $MODULES += "," + ($_ -replace "@.*")
 }
 
 # build Java Runtime with all the modules
 Write-Output "Building JRE to directory: .\$JRE_NAME"
-$JLINK --no-header-files --no-man-pages --compress=2 --module-path ${JDK_HOME}\jmods;${JAVAFX_JMODS_HOME} --add-modules ${MODULES} --output ${JRE_NAME}
+& $JLINK --no-header-files --no-man-pages --compress=2 --module-path "$Env:JDK_HOME\jmods;$Env:JAVAFX_JMODS_HOME" --add-modules ${MODULES} --output ${JRE_NAME}
 
 # package the Java Runtime as ZIP file
-$WORD_SIZE = (java -version 2>&1 | Select-String Bit | %{$_.Line.Split(' ')[1].Replace("-Bit", "");})
+$WORD_SIZE = (& $JAVA -version 2>&1 | Select-String Bit | %{$_.Line.Split(' ')[1].Replace("-Bit", "");})
 $JRE_ZIP = $JRE_NAME + "-with-jfx_windows-x" + $WORD_SIZE + ".zip" 
 
 Write-Output "Making JRE package: .\$JRE_ZIP"
--- src/windows/build-jre.ps1
+++ src/windows/build-jre.ps1
@@ -1,5 +1,5 @@
 # Environment variable: JDK_HOME must be set. You can uncomment and edit the line below to set JDK_HOME
-# $Env:JDK_HOME = "C:\Users\User\Downloads\openjdk-13.0.1"
+$Env:JDK_HOME = "C:\Users\wliu5\Downloads\jdk-13.0.1"
 
 if (-Not (Test-Path Env:JDK_HOME)) {
     Write-Output "Environment variable: JDK_HOME is not set."
@@ -7,38 +7,45 @@ if (-Not (Test-Path Env:JDK_HOME)) {
 }
 
 # check if java exists
-JAVA="$Env:JDK_HOME\bin\java"
+$JAVA="$Env:JDK_HOME\bin\java.exe"
 if (-Not (Test-Path $JAVA)) {
     Write-Output "$JAVA does not exist"
     Exit 3
 }
 
 # check if jlink exists
-JLINK="$Env:JDK_HOME\bin\jlink"
+$JLINK="$Env:JDK_HOME\bin\jlink.exe"
 if (-Not (Test-Path $JLINK)) {
     Write-Output "$JLINK does not exist"
     Exit 4
 }
 
+# 
+$Env:PATH = $Env:JDK_HOME + "\bin;" + $Env:PATH
+
 # parse Java version
-$VERSION_LINE = (java -version 2>&1 | Select-String version)
+$VERSION_LINE = (& $JAVA -version 2>&1 | Select-String version)
 $NAME = ($VERSION_LINE | %{$_.Line.Split(' ')[0];})
 $VERSION = ($VERSION_LINE | %{$_.Line.Split(' ')[2].Replace("`"", "");})
 $JRE_NAME = $NAME + "-" + $VERSION + "-jre"
 
 # get all moudles
 $MODULES = ""
-$JAVA --list-modules | ForEach-Object { 
+& $JAVA --list-modules | ForEach-Object { 
     $MODULES += ($_ -replace "@.*") + ","
 }
 $MODULES = ($MODULES -replace ",$")
 
 # build Java Runtime with all the modules
 Write-Output "Building JRE to directory: .\$JRE_NAME"
-$JLINK --no-header-files --no-man-pages --compress=2 --module-path ${JDK_HOME}\jmods --add-modules ${MODULES} --output ${JRE_NAME}
+& $JLINK --no-header-files --no-man-pages --compress=2 --module-path ${JDK_HOME}\jmods --add-modules ${MODULES} --output ${JRE_NAME}
+if (! $?) {
+    Write-Error "Error: failed to build the JRE with jlink"
+    exit 5
+}
 
 # package the Java Runtime as ZIP file
-$WORD_SIZE = (java -version 2>&1 | Select-String Bit | %{$_.Line.Split(' ')[1].Replace("-Bit", "");})
+$WORD_SIZE = (& $JAVA -version 2>&1 | Select-String Bit | %{$_.Line.Split(' ')[1].Replace("-Bit", "");})
 $JRE_ZIP = $JRE_NAME + "_windows-x" + $WORD_SIZE + ".zip" 
 
 Write-Output "Making JRE package: .\$JRE_ZIP"
